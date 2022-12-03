fc19e7722eb3fd3f2575193546424654746cd13c
yennic0302 <yender.yr45@gmail.com>
2022-12-03 15:06:00 -0400

    update client

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://yen-chat-back-production.up.railway.app";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
